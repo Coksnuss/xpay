6742154b6481935f0cbd23ed6d5e0efb35a5e1a9
Markus Schanz <coksnuss@googlemail.com>
2014-06-15 23:23:09 +0200

    Mail Konfiguration hinzugefügt. Funktioniert leider noch nicht wirklich. Mail an Marco ist aber bereits raus.

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,42 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'mail' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'messageConfig' => [
+                'charset' => 'UTF-8',
+                'from' => 'noreply@test.tld',
+            ],
+            'transport' => [
+                'class'      => 'Swift_SmtpTransport',
+                'host'       => 'mail.wsp.lab.sit.cased.de',
+                'username'   => 'xpay@wsp.lab.sit.cased.de',
+                'password'   => 'O}j[bmi[wb+E',
+                'port'       => '465',
+                'encryption' => 'ssl',
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        // Alternative mail account
+        'altMail' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class'      => 'Swift_SmtpTransport',
+                'host'       => 'mail.wsp.lab.sit.cased.de',
+                'username'   => 'ops1@wsp.lab.sit.cased.de',
+                'password'   => 'S@:zf>oi=e(R',
+                'port'       => '465',
+                'encryption' => 'ssl',
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
     ],
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'ops1@wsp.lab.sit.cased.de',
+    'supportEmail' => 'ops1@wsp.lab.sit.cased.d',
     'user.passwordResetTokenExpire' => 3600,
 ];
