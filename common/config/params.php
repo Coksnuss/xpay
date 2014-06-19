f0cb8db2a6f1e7f2938de152353b742087ef84cd
VM01 <vm01@lab.sit.cased.de>
2014-06-19 14:46:58 +0200

    Admin E-Mail angepasst

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'adminEmail' => 'ops1@wsp.lab.sit.cased.de',
-    'supportEmail' => 'ops1@wsp.lab.sit.cased.d',
+    'supportEmail' => 'ops1@wsp.lab.sit.cased.de',
     'user.passwordResetTokenExpire' => 3600,
 ];
