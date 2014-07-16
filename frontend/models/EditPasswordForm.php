fa7f3250118ab806e2796d69f07b142434bc840b
Rene Roepke <roepkix@googlemail.com>
2014-07-16 15:28:02 +0200

    passwordregeln hinzugefügt

--- frontend/models/EditPasswordForm.php
+++ frontend/models/EditPasswordForm.php
@@ -30,7 +30,7 @@ class EditPasswordForm extends Model
     		[['confirm_password'], 'compare', 'compareAttribute'=>'new_password'],
     		['current_password', 'validatePassword'],
     		[['confirm_password','new_password'],'string','min'=>'6'],
-    		//[['confirm_password','new_password'],'match', 'pattern'=>"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"],
+    		[['confirm_password','new_password'],'match', 'pattern'=>"((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"],
 			]
     		+ parent::rules();
     }
